4f1798094a568e187e1365fa64a9d7fc8e932ab8
NishiPhalke <30417924+NishiPhalke@users.noreply.github.com>
2018-10-04 15:49:55 -0400

    fetch trackhub service

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,7 +1,8 @@
 import { bigWigSchema } from "./bigwigSchema";
+import { trackHubSchema } from "./trackhubSchema";
 
-export const schema = bigWigSchema;
+//export const schema = bigWigSchema;
 
 // Use the following method when we have more than one schema
-// import { mergeSchemas } from 'graphql-tools';
-// export const schema = mergeSchemas({schemas: [bigWigSchema]});
+import { mergeSchemas } from "graphql-tools";
+export const schema = mergeSchemas({ schemas: [bigWigSchema, trackHubSchema] });
