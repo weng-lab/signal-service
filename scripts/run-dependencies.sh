1c1b94bb3240765c174393bfa1d72fb4b58b79ea
hpratt <hpratt111@live.com>
2018-11-01 13:57:49 -0400

    drop download from here for now, move to performance tests eventually

--- scripts/run-dependencies.sh
+++ scripts/run-dependencies.sh
@@ -3,8 +3,5 @@ set -e
 
 # cd to project root directory
 cd "$(dirname "$(dirname "$0")")"
-if [ ! -f resources/static/ENCFF686NUN.bigWig ]; then
-    wget https://www.encodeproject.org/files/ENCFF686NUN/@@download/ENCFF686NUN.bigWig -O resources/static/ENCFF686NUN.bigWig || true;
-fi
 
 docker-compose -f docker-compose.deps.yml up -d
