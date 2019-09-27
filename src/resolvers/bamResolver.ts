5706aec4d9b911ca85d5c4d140669dec0d1c281c
hpratt <hpratt111@live.com>
2019-09-27 19:45:17 -0400

    fix bug in bam resolver

--- src/resolvers/bamResolver.ts
+++ src/resolvers/bamResolver.ts
@@ -4,19 +4,22 @@ import { readBamIndex, BamIndexData } from "bigwig-reader/dist/bam/BamIndexReade
 import { readBamHeaderData } from "bigwig-reader/dist/bam/BamHeaderReader";
 import { BamAlignment, readBam } from "bigwig-reader";
 
+type BamIndexRequests = { requests: Array<BamIndexRequest>, googleProject: string };
+type BamRequests = { requests: Array<BamRequest>, googleProject: string };
+
 /**
  * Apollo server graphql resolver for batched bam index data requests.
  *
  * @param obj Unused. Needed by Apollo server resolver function signature.
  */
-async function bamIndexRequests(obj: any, { requests }: { requests: Array<BamIndexRequest> } | any): Promise<BamIndexResponse[]> {
-    return Promise.all(requests.map((request: BamIndexRequest) => bamIndexRequest(request)));
+async function bamIndexRequests(obj: any, { requests, googleProject }: BamIndexRequests | any): Promise<BamIndexResponse[]> {
+    return Promise.all(requests.map((request: BamIndexRequest) => bamIndexRequest(request, googleProject)));
 }
 
-async function bamIndexRequest(request: BamIndexRequest): Promise<BamIndexResponse> {
+async function bamIndexRequest(request: BamIndexRequest, googleProject?: string): Promise<BamIndexResponse> {
     const read = async (): Promise<BamIndexResponseData> => {
-        const bamIndexLoader = dataLoaderForArgs(request.baiUrl, request.googleProject);
-        const bamLoader = dataLoaderForArgs(request.bamUrl, request.googleProject);
+        const bamIndexLoader = dataLoaderForArgs(request.baiUrl, googleProject);
+        const bamLoader = dataLoaderForArgs(request.bamUrl, googleProject);
         const fullIndexData: BamIndexData = await readBamIndex(bamIndexLoader);
         const headerData = await readBamHeaderData(bamLoader, fullIndexData.firstAlignmentBlock);
         const refId = headerData.chromToId[request.chr];
@@ -26,13 +29,13 @@ async function bamIndexRequest(request: BamIndexRequest): Promise<BamIndexRespon
     return wrapRequest(read);
 }
 
-async function bamRequests(obj: any, { requests }: { requests: Array<BamRequest> } | any): Promise<BamResponse[]> {
-    return Promise.all(requests.map((request: BamRequest) => bamRequest(request)));
+async function bamRequests(obj: any, { requests, googleProject }: BamRequest | any): Promise<BamResponse[]> {
+    return Promise.all(requests.map((request: BamRequest) => bamRequest(request, googleProject)));
 }
 
-async function bamRequest(request: BamRequest): Promise<BamResponse> {
+async function bamRequest(request: BamRequest, googleProject?: string): Promise<BamResponse> {
     const read = async (): Promise<BamAlignment[]> => {
-        const bamLoader = dataLoaderForArgs(request.bamUrl, request.googleProject);
+        const bamLoader = dataLoaderForArgs(request.bamUrl, googleProject);
         return readBam(bamLoader, request.chunks, request.refId, request.chr, request.start, request.end);
     }
     return wrapRequest(read);
